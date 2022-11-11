6ac4716821d90fe2f331fe8fb54b8da59ce6562d
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-11-11 21:59:47 +0100

    HmacAuthenticationAttribute

---
+++ dotnet-hmac-authentication/ActionFilter/HmacAuthenticationAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace dotnet_hmac_authentication.ActionFilter
+{
+    public class HmacAuthenticationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+        }
+    }
+}
